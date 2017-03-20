13f5317daae62839f0a8034a4fd7babc731edab7
kylelowtharp <knlowtharp@gmail.com>
2017-03-20 11:48:31 -0500

    Add files via upload

---
+++ lis_results.py
@@ -0,0 +1,148 @@
+[<li class="b-head__item">
+<a class="b-head__menu-link" href="/statistics">
+              Stats
+            </a>
+</li>, <li class="b-head__item">
+<a class="b-head__menu-link" href="/data">
+              Feeds
+            </a>
+</li>, <li class="b-head__item">
+<a class="b-head__menu-link" href="/dev">
+              Dev
+            </a>
+</li>, <li class="b-head__item">
+<a class="b-head__menu-link" href="/company">
+              Company
+            </a>
+</li>, <li class="b-head__item">
+<a class="b-head__menu-link" href="/contact-us">
+              Contact Us
+            </a>
+</li>, <li class="b-head__item">
+<a class="b-head__menu-link" href="http://fmproduction.tv" target="_blank">
+          Broadcast + Prod. <img src="http://1e49bc5171d173577ecd-1323f4090557a33db01577564f60846c.r80.cf1.rackcdn.com/up_right_arrow.png"/>
+</a>
+</li>, <li class="b-statistics__tabs-item" data-tab="2">
+<a class="b-statistics__tabs-link" href="http://www.fightmetric.com/statistics/events/completed">
+        Events &amp; fights
+        <i class="b-statistics__tabs-corner b-statistics__tabs-corner_type_down"></i>
+</a>
+</li>, <li class="b-statistics__tabs-item b-statistics__tabs-item_state_current" data-tab="1">
+<a class="b-statistics__tabs-link " href="http://www.fightmetric.com/statistics/fighters">
+        Fighters
+        <i class="b-statistics__tabs-corner b-statistics__tabs-corner_type_down"></i>
+</a>
+</li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+        Height:
+      </i>
+      5' 11"
+    </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+        Weight:
+      </i>
+      170 lbs.
+    </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+        Reach:
+      </i>
+      72"
+    </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+        STANCE:
+      </i>
+      Switch
+    </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+        DOB:
+      </i>
+      
+        Jul 28, 1988
+      
+    </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            SLpM:
+          </i>
+
+          1.98
+
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            Str. Acc.:
+          </i>
+          56%
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            SApM:
+          </i>
+          3.68
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            Str. Def:
+          </i>
+          51%
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_type_width">
+</i>
+</li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            TD Avg.:
+          </i>
+          1.67
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            TD Acc.:
+          </i>
+          62%
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            TD Def.:
+          </i>
+          70%
+        </li>, <li class="b-list__box-list-item b-list__box-list-item_type_block">
+<i class="b-list__box-item-title b-list__box-item-title_font_lowercase b-list__box-item-title_type_width">
+            Sub. Avg.:
+          </i>
+          1.2
+        </li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/statistics">
+                  Stats
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/data">
+                  Feeds
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/dev">
+                  Dev
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/company">
+                  Company
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/contact-us">
+                  Contact
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="http://fmproduction.tv" target="_blank">
+                  FMProduction.TV
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/statistics">
+                  Events &amp; Fights
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/statistics/fighters">
+                  Fighters
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/fighter-stats">
+                  Stat Leaders
+                </a>
+</li>, <li class="b-foot__item">
+<a class="b-foot__link" href="/rankings">
+                  Rankings
+                </a>
+</li>]
+>>> 
